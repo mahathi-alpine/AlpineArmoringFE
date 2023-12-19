a0c53f9e6f3e884edfed2f7160aa79902cd8b559
Stefan <petkovics93@yahoo.com>
2023-12-19 12:44:17 +0100

    hp categories

--- next.config.js
+++ next.config.js
@@ -8,6 +8,6 @@ module.exports = {
     prependData: `@import './styles/_mixins.scss';`,
   },
   images: {
-    domains: ['assets.vercel.com', 'localhost', 'herokuapp.com', 'alpine-armoring-e7d249d45874.herokuapp.com'],
+    domains: ['res.cloudinary.com', 'assets.vercel.com', 'localhost', 'herokuapp.com', 'alpine-armoring-e7d249d45874.herokuapp.com'],
   },
 };
