c39d94d2e95a2d25815aebddf20b92be9c36c723
Stefan <petkovics93@yahoo.com>
2023-12-19 23:07:34 +0100

    introText

--- components/homepage/intro-text/IntroText.tsx
+++ components/homepage/intro-text/IntroText.tsx
@@ -1,19 +1,49 @@
-// import TextReveal from 'components/global/text-reveal/TextReveal';
 import styles from './IntroText.module.scss';
+import { useEffect, useRef} from 'react';
 
-// const IntroText = ({ props, error }) => {
 const IntroText = () => {
+  const introTextRef = useRef(null);
+
+  useEffect(() => {
+    let spans = [];
+    let htmlString = '';
+    let pArray = introTextRef.current.textContent.split('');
+    for(let i = 0; i < pArray.length; i++){
+      htmlString += `<span>${pArray[i]}</span>`;
+    }
+    introTextRef.current.innerHTML = htmlString;
+
+    spans = [...introTextRef.current.querySelectorAll('span')];
+
+    function revealSpans(){
+      for(let i = 0; i < spans.length; i++){
+        let {left, top} = spans[i].getBoundingClientRect();
+        top = top - (window.innerHeight * .7);
+        let opacityValue = 1 - ((top * .01) + (left * 0.001)) < 0.1 ? 0.1 : Number((1 - ((top * .01) + (left * 0.001))).toFixed(3));
+        opacityValue = opacityValue > 1 ? 1 : Number(opacityValue.toFixed(3));
+        spans[i].style.opacity = opacityValue;
+      }
+    }
+
+    window.addEventListener('scroll', () => {
+      if(introTextRef.current.getBoundingClientRect().top < (window.innerHeight / 2 + 300) && 
+      !(window.scrollY > (introTextRef.current.offsetTop + introTextRef.current.offsetHeight))){
+        revealSpans();
+      }
+    })
+
+  })
+
   return (
     <div className={`${styles.introText} container`}>
-      {/* <h3 className={`${styles.introText_subheading} observe animate fade-in-up`}>No one protects you better®</h3> */}
-
-      {/* <h2 className={`${styles.introText_heading} observe animate fade-in-up`}>Alpine Armoring</h2> */}
-
       {/* <TextReveal text="Alpine" /> */}
 
       <div className={`${styles.introText_content}`}>
-        <p className={`${styles.introText_text} observe animate fade-in-up`}>Alpine Armoring Inc., headquartered in the suburb of Washington D.C., is a high-quality custom-manufacturer of a variety of bulletproof armoured vehicles (armoured sedans, SUVs, pickup trucks, SWAT trucks and more).</p>
-        <h3 className={`${styles.introText_subheading} observe animate fade-in-up`}>
+        <p 
+          className={`${styles.introText_text} observe`} 
+          ref={introTextRef}
+        >Alpine Armoring Inc., headquartered in the suburb of Washington D.C., is a high-quality custom-manufacturer of a variety of bulletproof armoured vehicles (armoured sedans, SUVs, pickup trucks, SWAT trucks and more).</p>
+        <h3 className={`${styles.introText_subheading} observe`}>
           No one protects you better®
         </h3>
       </div>
