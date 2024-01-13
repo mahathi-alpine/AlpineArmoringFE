7dc1dbce9cac85d7709c587351713c17e69ed5de
Stefan <petkovics93@yahoo.com>
2024-01-13 21:56:25 +0100

    tabs

--- components/global/tab-slider/TabSlider.tsx
+++ components/global/tab-slider/TabSlider.tsx
@@ -42,7 +42,7 @@ const TabSlider = ({ props, onTabChange, className = '' }) => {
 
       const id = Array.from(navRef.current.children).indexOf(activeTab);
       onTabChange(id);
-    }, 100);
+    }, 300);
   }, [emblaApi]);
 
   useEffect(() => {
