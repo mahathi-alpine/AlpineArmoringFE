ba18ec899f9fa39a6f263b49794f56aa27aee2ec
Stefan <petkovics93@yahoo.com>
2024-02-19 17:37:37 +0100

    media

--- pages/media/videos/index.tsx
+++ pages/media/videos/index.tsx
@@ -2,9 +2,9 @@
 import { getPageData } from 'lib/api';
 import Banner from 'components/global/banner/Banner';
 import Seo from 'components/Seo';
-// import styles from './Videos.module.scss';
+import styles from './Videos.module.scss';
 // import EmblaCarousel from 'embla-carousel';
-// import VideoSingle from 'components/global/videos/VideoSingle';
+import VideoSingle from 'components/global/videos/VideoSingle';
 // import LightboxCustom from 'components/global/lightbox/LightboxCustom';
 
 function Videos(props) {
@@ -21,12 +21,36 @@ function Videos(props) {
     return acc;
   }, {});
 
-  console.log(groupedByCategory);
+  const handleLightboxOpen = () => {
+    console.log(groupedByCategory);
+  };
 
   return (
     <>
       <Seo props={seoData} />
       {banner ? <Banner props={banner} center shape="white" /> : null}
+
+      {Object.keys(groupedByCategory).map((category, index) => {
+        const itemsInCategory = groupedByCategory[category];
+
+        return (
+          <div className={`${styles.videos_list} container`} key={index}>
+            <h2 className={`${styles.videos_list_title}`}>{category}</h2>
+
+            <div className={`${styles.videos_list_slider} embla`}>
+              <div className={`${styles.videos_list_slider_inner}`}>
+                {itemsInCategory.map((item, index) => (
+                  <VideoSingle
+                    props={item}
+                    key={index}
+                    onVideoClick={handleLightboxOpen}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </>
   );
 }
