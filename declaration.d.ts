03e89bc719956d94d56fc69c16bfcb5e8d581566
Stefan <petkovics93@yahoo.com>
2024-04-21 15:37:02 +0200

    translator dynamic

--- declaration.d.ts
+++ declaration.d.ts
@@ -1,4 +1,12 @@
 declare module '*.scss' {
   const content: Record<string, string>;
+  declare global {
+    interface Window {
+      TranslateInit: () => void;
+      google: {
+        translate: any;
+      };
+    }
+  }
   export default content;
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,39 @@
 import '/styles/globals.scss';
 import Layout from '../components/Layout';
-import Script from 'next/script';
+// import Script from 'next/script';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   const canonicalUrl = `https://www.alpineco.com${router.pathname}`;
+
+  useEffect(() => {
+    // Check if the cookie exists
+    const cookieValue = getCookie('googtrans');
+    if (cookieValue) {
+      // Load the Google Translate script if the cookie is set
+      const script = document.createElement('script');
+      script.src =
+        '//translate.google.com/translate_a/element.js?cb=TranslateInit';
+      script.async = true;
+      script.onload = () => {
+        // Ensure the TranslateInit function is called after the script loads
+        window.TranslateInit = function () {
+          new window.google.translate.TranslateElement();
+        };
+        window.TranslateInit(); // Call the function immediately after the script loads
+      };
+      document.body.appendChild(script);
+    }
+  }, []);
 
   return (
     <>
@@ -18,19 +45,19 @@ export default function App({ Component, pageProps }) {
         strategy="lazyOnload"
         src="cookie, chatbots, newsletter"
       /> */}
-      <Script id="script-translate" strategy="afterInteractive">
+      {/* <Script id="script-translate" strategy="afterInteractive">
         {`
           window.TranslateInit = function () {
             new window.google.translate.TranslateElement();
           };        
         `}
-      </Script>
-      {process.env.GOOGLE_TRANSLATION_CONFIG && (
+      </Script> */}
+      {/* {process.env.GOOGLE_TRANSLATION_CONFIG && (
         <Script
           src="//translate.google.com/translate_a/element.js?cb=TranslateInit"
           strategy="afterInteractive"
         />
-      )}
+      )} */}
       <Head>
         <link rel="icon" href="/favicon.png" />
         <link rel="canonical" href={canonicalUrl} />
