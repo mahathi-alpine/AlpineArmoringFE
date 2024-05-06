ab5dc89790993b0ada9c6d283d19c30f2a3ccb31
Stefan <petkovics93@yahoo.com>
2024-05-06 17:45:26 +0200

    search

--- components/global/search/Search.tsx
+++ components/global/search/Search.tsx
@@ -1,4 +1,3 @@
-// import { useState, useEffect } from 'react';
 import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch } from 'react-instantsearch-dom';
 import styles from './Search.module.scss';
@@ -6,10 +5,6 @@ import SearchBox from './SearchBox';
 import SearchHits from './SearchHits';
 import { useRef } from 'react';
 import { useOutsideClick } from 'hooks/useOutsideClick';
-// const searchClient = algoliasearch(
-//   'BWXO30HFNW',
-//   'd152ea74492bb9a3eea96657ac73f8b8'
-// );
 
 const searchClient = algoliasearch(
   process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID,
@@ -24,19 +19,6 @@ export default function Search({ openSearchPopup }) {
     document.body.style.marginRight = '0';
     document.body.classList.remove('no-scroll');
   });
-
-  // const [searchClient, setSearchClient] = useState(null);
-
-  // useEffect(() => {
-  //   setSearchClient(algoliasearch(
-  //     process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID,
-  //     process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_API_KEY,
-  //   ));
-  // }, []);
-
-  // if (!searchClient) {
-  //   return null;
-  // }
 
   return (
     <div className={`${styles.search}`}>
