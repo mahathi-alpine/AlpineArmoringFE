af4d1d2a5b409ce46cb02b554fd2bf25749e88d6
Stefan <petkovics93@yahoo.com>
2024-05-08 17:16:50 +0200

    fixes

---
+++ pages/test/index.tsx
@@ -0,0 +1,19 @@
+export default function Custom404() {
+  return (
+    <>
+      <div className={`boxA`}>
+        1
+        <video
+          muted={true}
+          autoPlay={true}
+          playsInline={true}
+          loop={true}
+          width={1920}
+          height={1200}
+          preload="auto"
+          src={`https://d102sycao8uwt8.cloudfront.net/Mainpage_5_8pre_release_mobile_1_da298c14ac.mp4`}
+        ></video>
+      </div>
+    </>
+  );
+}
