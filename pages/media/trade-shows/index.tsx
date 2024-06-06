5e8dbfb3558235e7631b6e035cb726d379a05b66
mahathi-alpine <mahathi.vantedhu@alpineco.com>
2024-06-06 08:46:28 -0400

    trade shows sort

--- pages/media/trade-shows/index.tsx
+++ pages/media/trade-shows/index.tsx
@@ -54,6 +54,7 @@ export async function getStaticProps() {
   let tradeShows = await getPageData({
     route: 'trade-shows',
     populate: 'deep',
+    sort: 'date',
     pageSize: 100,
   });
   tradeShows = tradeShows?.data || null;
