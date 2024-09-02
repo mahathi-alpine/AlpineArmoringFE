81efef43d5d1502f1338e66ea1a911d729a767d0
Stefan <petkovics93@yahoo.com>
2024-09-02 18:24:37 +0200

    pdf popup

--- components/global/lightbox/PdfViewer2.jsx
+++ components/global/lightbox/PdfViewer2.jsx
@@ -1,17 +1,16 @@
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 // import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
-
 import '@react-pdf-viewer/core/lib/styles/index.css';
 // import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 const PdfViewer = ({ pdfUrl }) => {
-  // const defaultLayoutPluginInstance = defaultLayoutPlugin; 
+  // const defaultLayoutPluginInstance = defaultLayoutPlugin;
   // const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
   return (
     <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-      <div style={{ height: '100vh', width: '100%'}}>
+      <div style={{ height: '100vh', width: '100%' }}>
         <Viewer
           fileUrl={pdfUrl}
           defaultScale={1.2}
