cdeecb2abebaaf355e4b2c5052e7c6f734499d7e
mahathi-alpine <mahathi.vantedhu@alpineco.com>
2024-09-12 15:13:00 -0400

    landing inquiry fix

--- components/global/form/LandingInquiryForm.tsx
+++ components/global/form/LandingInquiryForm.tsx
@@ -1,4 +1,4 @@
-import styles from './InquiryForm.module.scss';
+import styles from './LandingInquiryForm.module.scss';
 import Form from 'components/global/form/Form';
 
 const LandingInquiryForm = (props) => {
@@ -16,11 +16,8 @@ const LandingInquiryForm = (props) => {
         <div className={`shapeCurved_leftBottom shapeCurved`}></div>
         <div className={`${styles.inquiry_form_heading}`}>
           <span>
-            <strong>FOR MORE INFORMATION:</strong>
+            <strong>FOR MORE INFORMATION, PLEASE COMPLETE THIS FORM:</strong>
           </span>
-          <br />
-          <br />
-          Call +1 (703) 471-0002 or complete the form below
         </div>
         <div className={`${styles.inquiry_form_inner} container_small`}>
           <Form />
