53892d7fcb5a21e5da6a2a1ec67321a0a15cb81c
Stefan <petkovics93@yahoo.com>
2024-09-23 16:43:37 +0200

    vwa multiple categories, sorting

--- pages/vehicles-we-armor/index.tsx
+++ pages/vehicles-we-armor/index.tsx
@@ -9,83 +9,90 @@ import { useEffect, useState } from 'react';
 
 function VehicleWeArmor(props) {
   const router = useRouter();
-  const { q } = router.query;
+  // const { q } = router.query;
   const topBanner = props.pageData?.banner;
 
   const [vehiclesData, setVehiclesData] = useState(props.vehicles.data);
+  const [itemsToRender, setItemsToRender] = useState(12);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setVehiclesData(props.vehicles.data);
   }, [router.query]);
 
-  const categoryOrderMap = new Map(
-    props.filters.type?.map((category) => [
-      category.attributes.title,
-      category.attributes.order,
-    ])
-  );
-
-  const vehiclesArray = vehiclesData?.sort((a, b) => {
-    // Correctly access the category order using the category title
-    const categoryOrderA = categoryOrderMap.get(
-      a.attributes.category.data?.attributes.title
-    );
-    const categoryOrderB = categoryOrderMap.get(
-      b.attributes.category.data?.attributes.title
-    );
-
-    // Compare by category order
-    if (categoryOrderA !== categoryOrderB) {
-      return Number(categoryOrderA) - Number(categoryOrderB);
+  const fetchMoreItems = () => {
+    if (itemsToRender < vehiclesData?.length) {
+      setLoading(true);
+      setItemsToRender((prevItemsToRender) => prevItemsToRender + 12);
+      setLoading(false);
     }
-
-    // If categories are equal, compare by make title alphabetically
-    const makeOrderA = a.attributes.make
-      ? a.attributes.make.data?.attributes.title
-      : '';
-    const makeOrderB = b.attributes.make
-      ? b.attributes.make.data?.attributes.title
-      : '';
-    return makeOrderA?.localeCompare(makeOrderB);
-  });
-
-  const [currentPage, setCurrentPage] = useState(2);
-  const [hasMore, setHasMore] = useState(true);
-
-  const [loading, setLoading] = useState(false);
-
-  const pageLimit = 30;
-
-  const fetchMoreItems = async () => {
-    if (!hasMore) return;
-
-    const totalItems = props.vehicles?.meta?.pagination?.total || 0;
-
-    if (currentPage * pageLimit >= totalItems + pageLimit) return;
-
-    setLoading(true);
-    setCurrentPage(currentPage + 1);
-
-    const query = q ? `filters[slug][$contains]=${q}` : '';
-
-    // Fetch the next batch of items using the current page number
-    const vehicles = await getPageData({
-      route: 'vehicles-we-armors',
-      sort: 'category.order',
-      populate: 'featuredImage, category, make',
-      page: currentPage,
-      pageSize: pageLimit,
-      params: query,
-    });
-
-    if (Array.isArray(vehicles.data)) {
-      setVehiclesData((prevData) => [...prevData, ...vehicles.data]);
-    }
-
-    const itemsFetched = vehiclesData?.length + vehicles.data.length || 0;
-    setHasMore(itemsFetched < totalItems);
-    setLoading(false);
   };
+
+  // const categoryOrderMap = new Map(
+  //   props.filters.type?.map((category) => [
+  //     category.attributes.title,
+  //     category.attributes.order,
+  //   ])
+  // );
+
+  // const vehiclesArray = vehiclesData?.sort((a, b) => {
+  //   // Correctly access the category order using the category title
+  //   const categoryOrderA = categoryOrderMap.get(
+  //     a.attributes.category.data?.attributes.title
+  //   );
+  //   const categoryOrderB = categoryOrderMap.get(
+  //     b.attributes.category.data?.attributes.title
+  //   );
+
+  //   // Compare by category order
+  //   if (categoryOrderA !== categoryOrderB) {
+  //     return Number(categoryOrderA) - Number(categoryOrderB);
+  //   }
+
+  //   // If categories are equal, compare by make title alphabetically
+  //   const makeOrderA = a.attributes.make
+  //     ? a.attributes.make.data?.attributes.title
+  //     : '';
+  //   const makeOrderB = b.attributes.make
+  //     ? b.attributes.make.data?.attributes.title
+  //     : '';
+  //   return makeOrderA?.localeCompare(makeOrderB);
+  // });
+
+  // const [currentPage, setCurrentPage] = useState(2);
+  // const [hasMore, setHasMore] = useState(true);
+  // const pageLimit = 30;
+
+  // const fetchMoreItems = async () => {
+  //   if (!hasMore) return;
+
+  //   const totalItems = props.vehicles?.meta?.pagination?.total || 0;
+
+  //   if (currentPage * pageLimit >= totalItems + pageLimit) return;
+
+  //   setLoading(true);
+  //   setCurrentPage(currentPage + 1);
+
+  //   const query = q ? `filters[slug][$contains]=${q}` : '';
+
+  //   // Fetch the next batch of items using the current page number
+  //   const vehicles = await getPageData({
+  //     route: 'vehicles-we-armors',
+  //     sort: 'category.order',
+  //     populate: 'featuredImage, category, make',
+  //     page: currentPage,
+  //     pageSize: pageLimit,
+  //     params: query,
+  //   });
+
+  //   if (Array.isArray(vehicles.data)) {
+  //     setVehiclesData((prevData) => [...prevData, ...vehicles.data]);
+  //   }
+
+  //   const itemsFetched = vehiclesData?.length + vehicles.data.length || 0;
+  //   setHasMore(itemsFetched < totalItems);
+  //   setLoading(false);
+  // };
 
   useEffect(() => {
     const targets = document.querySelectorAll('.observe');
@@ -109,7 +116,8 @@ function VehicleWeArmor(props) {
       targets.forEach((item) => observer.unobserve(item));
       observer.disconnect();
     };
-  }, [currentPage, hasMore, props.vehicles.data]);
+    // }, [currentPage, hasMore, props.vehicles.data]);
+  }, [itemsToRender, vehiclesData]);
 
   return (
     <>
@@ -121,18 +129,18 @@ function VehicleWeArmor(props) {
         </div>
 
         <div className={`${styles.listing_wrap} container`}>
-          {vehiclesArray?.length < 1 ? (
+          {vehiclesData?.length < 1 ? (
             <div className={`${styles.listing_empty}`}>
               <h2>No Vehicles Found</h2>
             </div>
           ) : null}
 
-          {vehiclesArray ? (
+          {vehiclesData ? (
             <div className={`${styles.listing_list}`}>
-              {vehiclesArray.map((vehicle, index) => (
+              {vehiclesData.slice(0, itemsToRender).map((item, index) => (
                 <InventoryItem
                   key={index}
-                  props={vehicle}
+                  props={item}
                   index={index === 0 ? index : 1}
                 />
               ))}
@@ -154,55 +162,77 @@ function VehicleWeArmor(props) {
 }
 
 export async function getServerSideProps(context) {
-  let pageData = await getPageData({
-    route: 'list-vehicles-we-armor',
-    populate: 'deep',
-  });
-  pageData = pageData?.data?.attributes || null;
+  try {
+    let pageData = await getPageData({
+      route: 'list-vehicles-we-armor',
+      populate: 'deep',
+    });
+    pageData = pageData?.data?.attributes || null;
 
-  let query = '';
-  if (context.query.category) {
-    query += `&filters[category][slug][$eq]=${context.query.category}`;
-  }
-  if (context.query.make) {
-    query += `&filters[make][slug][$eq]=${context.query.make}`;
-  }
-  if (context.query.q) {
-    query += `filters[slug][$contains]=${context.query.q.toLowerCase()}`;
-  }
-  const vehicles = await getPageData({
-    route: 'vehicles-we-armors',
-    params: query,
-    populate: 'featuredImage, category, make',
-    page: 1,
-    pageSize: 30,
-    sort: 'category.order',
-  });
+    let query = '';
+    if (context.query.category) {
+      query += `&filters[category][slug][$eq]=${context.query.category}`;
+    }
+    if (context.query.make) {
+      query +=
+        (query ? '&' : '') + `&filters[make][slug][$eq]=${context.query.make}`;
+    }
+    if (context.query.q) {
+      query += (query ? '&' : '') + `filters[slug][$notNull]=true`;
+    }
 
-  const [type, make] = await Promise.all([
-    getPageData({
-      route: 'categories',
-      sort: 'order',
-      fields: 'fields[0]=title&fields[1]=slug&fields[2]=order',
-    }).then((res) => res.data),
-    getPageData({
-      route: 'makes',
-      sort: 'title',
+    const vehicles = await getPageData({
+      route: 'vehicles-we-armors',
+      params: query,
+      populate: 'featuredImage, category, make',
       pageSize: 100,
-      fields: 'fields[0]=title&fields[1]=slug',
-    }).then((res) => res.data),
-  ]);
+      sort: 'order',
+    });
 
-  let filters = {};
-  if (type && make) {
-    filters = { type, make };
+    const filteredVehicles = {
+      ...vehicles,
+      data: vehicles.data.filter((vehicle) => {
+        if (!context.query.q) return true;
+
+        const searchTerms = context.query.q.toLowerCase().replace(/[-\s]/g, '');
+        const slug = vehicle.attributes.slug
+          .toLowerCase()
+          .replace(/[-\s]/g, '');
+
+        return slug.includes(searchTerms);
+      }),
+    };
+
+    const [type, make] = await Promise.all([
+      getPageData({
+        route: 'categories',
+        sort: 'order',
+        fields: 'fields[0]=title&fields[1]=slug&fields[2]=order',
+      }).then((res) => res.data),
+      getPageData({
+        route: 'makes',
+        sort: 'title',
+        pageSize: 100,
+        fields: 'fields[0]=title&fields[1]=slug',
+      }).then((res) => res.data),
+    ]);
+
+    let filters = {};
+    if (type && make) {
+      filters = { type, make };
+    }
+
+    const seoData = pageData?.seo || null;
+
+    return {
+      props: { pageData, vehicles: filteredVehicles, filters, seoData },
+    };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return {
+      notFound: true,
+    };
   }
-
-  const seoData = pageData?.seo || null;
-
-  return {
-    props: { pageData, vehicles, filters, seoData },
-  };
 }
 
 export default VehicleWeArmor;
--- pages/vehicles-we-armor/type/[type].tsx
+++ pages/vehicles-we-armor/type/[type].tsx
@@ -1,57 +1,69 @@
 import { getPageData } from 'hooks/api';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-// import Link from 'next/link';
 import Banner from 'components/global/banner/Banner';
 import Filters from 'components/listing/filters/Filters';
 import InventoryItem from 'components/listing/listing-item-all/ListingItemAll';
 import styles from '/components/listing/Listing.module.scss';
 
 function Inventory(props) {
-  const router = useRouter();
-  const { make, q } = router.query;
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    if (router.isReady) {
-      setIsLoading(false);
-    }
-  }, [router.isReady]);
-
   let topBanner = props.filters?.type?.find(
     (item) => item.attributes.slug === props.query
   );
   topBanner = topBanner?.attributes.allBanner;
 
-  // Filtering vehicles based on the make parameter
+  const router = useRouter();
+
+  const [vehiclesData, setVehiclesData] = useState(props.vehicles.data);
+  const [itemsToRender, setItemsToRender] = useState(6);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setVehiclesData(props.vehicles.data);
+  }, [router.query]);
+
+  const fetchMoreItems = () => {
+    if (itemsToRender < vehiclesData?.length) {
+      setLoading(true);
+      setItemsToRender((prevItemsToRender) => prevItemsToRender + 6);
+      setLoading(false);
+    }
+  };
+
+  // // Filtering vehicles based on the make parameter
   const filteredByMake = props.vehicles?.data?.filter(
     (vehicle) =>
-      !make || vehicle.attributes.make?.data?.attributes?.slug === make
+      !router.query.make ||
+      vehicle.attributes.make?.data?.attributes?.slug === router.query.make
   );
 
-  // Filtering vehicles based on the q parameter
-  const filteredByQ = filteredByMake?.filter(
-    (vehicle) => !q || vehicle.attributes?.slug.includes(q)
-  );
+  // // Filtering vehicles based on the q parameter
+  // const filteredByQ = filteredByMake?.filter(
+  //   (vehicle) => !q || vehicle.attributes?.slug.includes(q)
+  // );
+
+  // useEffect(() => {
+  //   if (router.isReady) {
+  //     setLoading(false);
+  //   }
+  // }, [router.isReady]);
 
   // Animations
   useEffect(() => {
     const targets = document.querySelectorAll('.observe');
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.toggle('in-view', entry.isIntersecting);
-            observer.unobserve(entry.target);
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.toggle('in-view', entry.isIntersecting);
+          observer.unobserve(entry.target);
+
+          if (entry.target.classList.contains('bottomObserver')) {
+            fetchMoreItems();
           }
-        });
-      },
-      {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.2,
-      }
-    );
+        }
+      });
+    });
 
     targets.forEach((item) => observer.observe(item));
 
@@ -59,7 +71,7 @@ function Inventory(props) {
       targets.forEach((item) => observer.unobserve(item));
       observer.disconnect();
     };
-  }, []);
+  }, [itemsToRender]);
 
   // const findTitleBySlug = (filters, targetSlug) => {
   //   if (!filters || !Array.isArray(filters.type)) {
@@ -94,7 +106,17 @@ function Inventory(props) {
         ) : null}
 
         <div className={`${styles.listing_wrap} container`}>
-          {filteredByQ?.length < 1 ? (
+          {filteredByMake?.length > 0 && !loading ? (
+            <div className={`${styles.listing_list}`}>
+              {filteredByMake.map((item, index) => (
+                <InventoryItem key={item.id} props={item} index={index} />
+              ))}
+            </div>
+          ) : (
+            <div className={`${styles.listing_empty}`}>No Vehicles Found</div>
+          )}
+
+          {/* {filteredByQ?.length < 1 ? (
             <div className={`${styles.listing_empty}`}>
               <h2>No Vehicles Found</h2>
             </div>
@@ -106,53 +128,83 @@ function Inventory(props) {
                 <InventoryItem key={item.id} props={item} index={index} />
               ))}
             </div>
-          ) : null}
+          ) : null} */}
         </div>
       </div>
+
+      <div
+        className={`${styles.listing_loading} ${styles.listing_loading_stock}`}
+        style={{ opacity: loading ? 1 : 0 }}
+      >
+        Loading...
+      </div>
+
+      <div className={`observe bottomObserver`}></div>
     </>
   );
 }
 
-export async function getStaticPaths() {
-  try {
-    const slugsResponse = await getPageData({
-      route: 'categories',
-      fields: 'fields[0]=slug',
-      populate: '/',
-    });
+// export async function getStaticPaths() {
+//   try {
+//     const slugsResponse = await getPageData({
+//       route: 'categories',
+//       fields: 'fields[0]=slug',
+//       populate: '/',
+//     });
 
-    if (!Array.isArray(slugsResponse.data)) {
-      throw new Error('Invalid data format');
-    }
+//     if (!Array.isArray(slugsResponse.data)) {
+//       throw new Error('Invalid data format');
+//     }
 
-    const paths = slugsResponse.data.reduce((acc, item) => {
-      if (item.attributes && item.attributes.slug) {
-        acc.push({ params: { type: item.attributes.slug } });
-      }
-      return acc;
-    }, []);
+//     const paths = slugsResponse.data.reduce((acc, item) => {
+//       if (item.attributes && item.attributes.slug) {
+//         acc.push({ params: { type: item.attributes.slug } });
+//       }
+//       return acc;
+//     }, []);
 
-    return {
-      paths,
-      fallback: true,
-    };
-  } catch (error) {
-    console.error('Error fetching slugs:', error);
-    return {
-      paths: [],
-      fallback: false,
-    };
+//     return {
+//       paths,
+//       fallback: true,
+//     };
+//   } catch (error) {
+//     console.error('Error fetching slugs:', error);
+//     return {
+//       paths: [],
+//       fallback: false,
+//     };
+//   }
+// }
+
+export async function getServerSideProps(context) {
+  // Fetching Vehicles
+  const category = context.query.type;
+  let query = `filters[category][slug][$eq]=${category}`;
+  const q = context.query.q;
+  if (q) {
+    query += (query ? '&' : '') + `filters[slug][$notNull]=true`;
   }
-}
 
-export async function getStaticProps({ params }) {
   const vehicles = await getPageData({
     route: 'vehicles-we-armors',
-    params: `filters[category][slug][$eq]=${params.type}`,
-    sort: 'title',
+    // params: `filters[category][slug][$eq]=${params.type}`,
+    params: query,
+    sort: 'order',
     pageSize: 100,
     populate: 'featuredImage, make',
   });
+
+  const filteredVehicles = {
+    ...vehicles,
+    data: vehicles.data.filter((vehicle) => {
+      if (!q) return true;
+
+      const searchTerms = q.toLowerCase().replace(/[-\s]/g, '');
+      const slug = vehicle.attributes.slug.toLowerCase().replace(/[-\s]/g, '');
+
+      return slug.includes(searchTerms);
+    }),
+  };
 
   // Fetching Types and Makes for the Filters
   const [type, make] = await Promise.all([
@@ -175,11 +227,18 @@ export async function getStaticProps({ params }) {
     filters = { type, make };
   }
 
-  let seoData = type?.find((item) => item.attributes.slug === params.type);
+  let seoData = type?.find(
+    (item) => item.attributes.slug === context.query.type
+  );
   seoData = seoData?.attributes.seo || null;
 
   return {
-    props: { vehicles, filters, query: params.type, seoData },
+    props: {
+      vehicles: filteredVehicles,
+      filters,
+      query: context.query.type,
+      seoData,
+    },
   };
 }
 
