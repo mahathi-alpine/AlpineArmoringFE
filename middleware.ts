9bff79ec87370066b81d323f49d3385c498cb1d1
Stefan <petkovics93@yahoo.com>
2024-11-19 14:59:33 +0100

    noindex middleware

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  if (
+    request.nextUrl.pathname.startsWith('/stock') ||
+    request.nextUrl.pathname.startsWith('/inventory')
+  ) {
+    const url = request.nextUrl.clone();
+    url.pathname = '/';
+
+    const response = NextResponse.redirect(url);
+    response.headers.set('X-Robots-Tag', 'noindex, nofollow');
+    return response;
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/stock/:path*', '/inventory/:path*'],
+};
