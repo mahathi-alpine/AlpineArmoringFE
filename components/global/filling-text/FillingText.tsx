4346b7dd7480ab51ab6c7a5c717e1ada52be94c4
Stefan <petkovics93@yahoo.com>
2025-01-29 16:24:01 +0100

    fillingText static

--- components/global/filling-text/FillingText.tsx
+++ components/global/filling-text/FillingText.tsx
@@ -1,60 +1,63 @@
 import styles from './FillingText.module.scss';
-import { useRef } from 'react';
-import useEffectOnce from 'hooks/useEffectOnce';
+import { useMarkdownToHtml } from 'hooks/useMarkdownToHtml';
+// import { useRef } from 'react';
+// import useEffectOnce from 'hooks/useEffectOnce';
 
 const FillingText = ({ data, dark = false, className = '', small = false }) => {
-  const fillingTextRef = useRef(null);
+  // const fillingTextRef = useRef(null);
 
   const title = data.title;
   const text = data.text || data;
+  const convertMarkdown = useMarkdownToHtml();
+  const wrappedContent = `<p>${convertMarkdown(text || '')}</p>`;
 
-  const revealSpans = () => {
-    const spans = [...fillingTextRef.current.querySelectorAll('span')];
+  // const revealSpans = () => {
+  //   const spans = [...fillingTextRef.current.querySelectorAll('span')];
 
-    for (let i = 0; i < spans.length; i++) {
-      const left = spans[i].getBoundingClientRect().left;
-      let top = spans[i].getBoundingClientRect().top;
-      top = top - window.innerHeight * 0.7;
-      let opacityValue =
-        1 - (top * 0.01 + left * 0.001) < 0.1
-          ? 0.1
-          : Number((1 - (top * 0.01 + left * 0.001)).toFixed(3));
-      opacityValue = opacityValue > 1 ? 1 : Number(opacityValue.toFixed(3));
-      spans[i].style.opacity = opacityValue;
-    }
-  };
+  //   for (let i = 0; i < spans.length; i++) {
+  //     const left = spans[i].getBoundingClientRect().left;
+  //     let top = spans[i].getBoundingClientRect().top;
+  //     top = top - window.innerHeight * 0.7;
+  //     let opacityValue =
+  //       1 - (top * 0.01 + left * 0.001) < 0.1
+  //         ? 0.1
+  //         : Number((1 - (top * 0.01 + left * 0.001)).toFixed(3));
+  //     opacityValue = opacityValue > 1 ? 1 : Number(opacityValue.toFixed(3));
+  //     spans[i].style.opacity = opacityValue;
+  //   }
+  // };
 
-  useEffectOnce(() => {
-    if (typeof window !== 'undefined') {
-      if (window.innerWidth > 1280) {
-        const pArray = fillingTextRef.current?.textContent.split('');
-        if (pArray) {
-          let htmlString = '';
+  // useEffectOnce(() => {
+  //   if (typeof window !== 'undefined') {
+  //     if (window.innerWidth > 1280) {
+  //       const pArray = fillingTextRef.current?.textContent.split('');
+  //       if (pArray) {
+  //         let htmlString = '';
 
-          for (let i = 0; i < pArray.length; i++) {
-            htmlString += `<span>${pArray[i]}</span>`;
-          }
-          fillingTextRef.current.innerHTML = htmlString;
-        }
+  //         for (let i = 0; i < pArray.length; i++) {
+  //           htmlString += `<span>${pArray[i]}</span>`;
+  //         }
+  //         fillingTextRef.current.innerHTML = htmlString;
+  //       }
 
-        window.addEventListener('scroll', () => {
-          if (fillingTextRef.current) {
-            if (
-              fillingTextRef.current.getBoundingClientRect().top <
-                window.innerHeight / 2 + 300 &&
-              !(
-                window.scrollY >
-                fillingTextRef.current.offsetTop +
-                  fillingTextRef.current.offsetHeight
-              )
-            ) {
-              revealSpans();
-            }
-          }
-        });
-      }
-    }
-  });
+  //       window.addEventListener('scroll', () => {
+  //         if (fillingTextRef.current) {
+  //           if (
+  //             fillingTextRef.current.getBoundingClientRect().top <
+  //               window.innerHeight / 2 + 300 &&
+  //             !(
+  //               window.scrollY >
+  //               fillingTextRef.current.offsetTop +
+  //                 fillingTextRef.current.offsetHeight
+  //             )
+  //           ) {
+  //             revealSpans();
+  //           }
+  //         }
+  //       });
+  //     }
+  //   }
+  // });
 
   return (
     <div
@@ -75,11 +78,13 @@ const FillingText = ({ data, dark = false, className = '', small = false }) => {
         ) : null}
 
         {text ? (
-          <p
+          <div
             className={`${styles.fillingText_text} observe`}
-            dangerouslySetInnerHTML={{ __html: text }}
-            ref={fillingTextRef}
-          ></p>
+            // ref={fillingTextRef}
+            dangerouslySetInnerHTML={{
+              __html: convertMarkdown(wrappedContent),
+            }}
+          ></div>
         ) : null}
       </div>
     </div>
