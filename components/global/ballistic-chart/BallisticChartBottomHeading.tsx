2730180d3360e8b52ac22d49adb6193306f88e95
Stefan <petkovics93@yahoo.com>
2025-03-03 16:57:30 +0100

    pdf zoom

--- components/global/ballistic-chart/BallisticChartBottomHeading.tsx
+++ components/global/ballistic-chart/BallisticChartBottomHeading.tsx
@@ -1,25 +1,41 @@
 import styles from './BallisticChartBottom.module.scss';
 import Image from 'next/image';
-import useLightbox from '../lightbox/useLightbox';
-import NextJsImage from '../lightbox/NextJsImage';
-import Zoom from 'yet-another-react-lightbox/plugins/zoom';
+import { useState } from 'react';
+
+import dynamic from 'next/dynamic';
+const PopupPDF = dynamic(() => import('components/global/lightbox/PopupPDF'), {
+  ssr: false,
+});
+// import useLightbox from '../lightbox/useLightbox';
+// import NextJsImage from '../lightbox/NextJsImage';
+// import Zoom from 'yet-another-react-lightbox/plugins/zoom';
 
 const BallisticChartBottomHeading = (props) => {
-  const { openLightbox, renderLightbox } = useLightbox();
+  // const { openLightbox, renderLightbox } = useLightbox();
 
-  type CustomSlide = {
-    src: string;
-    width?: number;
-    height?: number;
-    alt?: string;
-    unoptimized?: boolean;
-    index?: number;
-    selectedIndex?: number;
+  // type CustomSlide = {
+  //   src: string;
+  //   width?: number;
+  //   height?: number;
+  //   alt?: string;
+  //   unoptimized?: boolean;
+  //   index?: number;
+  //   selectedIndex?: number;
+  // };
+  const [isPDFPopupOpen, setPDFPopupOpen] = useState(false);
+  const [currentPdfUrl, setCurrentPdfUrl] = useState('');
+
+  const togglePDFPopup = (url) => {
+    setPDFPopupOpen((prevState) => !prevState);
+    setCurrentPdfUrl(url);
   };
 
   return (
     <>
-      <div className={`${styles.ballistic_bottom_top}`}>
+      <div
+        className={`${styles.ballistic_bottom_top}`}
+        id="ballistic-chart-top"
+      >
         <Image
           src="/assets/ballistic/ballistic_logo_big.png"
           alt="Alpine Armoring"
@@ -36,16 +52,23 @@ const BallisticChartBottomHeading = (props) => {
           </h2>
           <button
             className={`${styles.ballistic_bottom_top_content_button}`}
-            onClick={() => {
-              openLightbox();
-            }}
+            // onClick={() => {
+            //   openLightbox();
+            // }}
+            onClick={() => togglePDFPopup(props.bulletPoster)}
           >
             Discover
           </button>
         </div>
       </div>
 
-      {renderLightbox({
+      <PopupPDF
+        isOpen={isPDFPopupOpen}
+        onClose={() => togglePDFPopup('')}
+        pdfUrl={currentPdfUrl}
+      />
+
+      {/* {renderLightbox({
         slides: [
           {
             src: props.bulletPoster?.url,
@@ -61,7 +84,7 @@ const BallisticChartBottomHeading = (props) => {
           buttonPrev: () => null,
           buttonNext: () => null,
         },
-      })}
+      })} */}
     </>
   );
 };
--- components/global/lightbox/PdfViewer.jsx
+++ components/global/lightbox/PdfViewer.jsx
@@ -1,9 +1,32 @@
 import { Viewer, Worker } from '@react-pdf-viewer/core';
+import { zoomPlugin } from '@react-pdf-viewer/zoom';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/zoom/lib/styles/index.css';
 
 const PdfViewer = ({ pdfUrl }) => {
+
+  const zoomPluginInstance = zoomPlugin();
+  const { ZoomInButton, ZoomOutButton, ZoomPopover } = zoomPluginInstance;
+
   return (
     <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-      <Viewer fileUrl={pdfUrl} defaultScale={1.2} />
+      
+      <div
+          style={{
+              alignItems: 'center',
+              backgroundColor: '#eeeeee',
+              display: 'flex',
+              justifyContent: 'center',
+              padding: '2px',
+              fontSize: '14px',
+          }}
+      >
+          <ZoomOutButton />
+          <ZoomPopover />
+          <ZoomInButton />
+      </div>
+      <Viewer fileUrl={pdfUrl} defaultScale={1.2} plugins={[zoomPluginInstance]} />
+   
     </Worker>
   );
 };
