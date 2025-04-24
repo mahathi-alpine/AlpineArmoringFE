a07ba7a6d88cdfd707fda53e2ba713701ead3fd8
Stefan <petkovics93@yahoo.com>
2025-04-24 15:20:34 +0200

    contentDispositionType inline

--- next.config.js
+++ next.config.js
@@ -44,6 +44,7 @@ module.exports = {
 
   images: {
     // unoptimized: true,
+    contentDispositionType: 'inline',
     deviceSizes: [384, 640, 750, 828, 1080, 1200, 1920, 2200],
     remotePatterns: [
       {
